c7b419086bce36d81a35bb49805aff54ff11e273
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-01-14 23:47:49 -0600

    first commit

---
+++ controllers/recipes.js
@@ -0,0 +1,87 @@
+const cloudinary = require("../middleware/cloudinary");
+const Recipe = require("../models/Recipe");
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      //Since we have a session each request (req) contains the logged-in users info: req.user
+      //Grabbing just the recipes of the logged-in user
+      //Console.log (req.user) to see everything
+      const recipes = await Recipe.find({ user: req.user.id });
+      //Sending recipe data from mongodb and user data to ejs template
+      res.render("profile.ejs", { recipes: recipes, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getFeed: async (req, res) => {
+    try {
+      const recipes = await Recipe.find().sort({ createdAt: "desc" }).lean();
+      res.render("feed.ejs", { recipes: recipes });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getRecipe: async (req, res) => {
+    try {
+      //id param comes from Recipe routes
+      //router.get("/:id", ensureAuth, recipesController.getrecipe);
+      //example url: http://localhost:2121/recipe/01293847a;osidjf
+      //id === 01293847a;osidjf
+      const recipe = await Recipe.findById(req.params.id);
+      res.render("recipe.ejs", { recipe: recipe, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createRecipe: async (req, res) => {
+    try {
+      console.log('beta')
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      //media stored on cloudinary - above request responds with url and the media id that you will need when deleting content
+      await Recipe.create({
+        name: req.body.name,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        ingredients: req.body.ingredients,
+        directions: req.body.directions,
+        likes: 0,
+        user: req.user.id,
+      });
+      console.log("Recipe has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeRecipe: async (req, res) => {
+    try {
+      await Recipe.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/recipe/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteRecipe: async (req, res) => {
+    try {
+      // Find recipe by id
+      let recipe = await Recipe.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(recipe.cloudinaryId);
+      // Delete recipe from db
+      await Recipe.remove({ _id: req.params.id });
+      console.log("Deleted Recipe");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};
---
+++ routes/recipes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const recipesController = require("../controllers/recipes");
+const { ensureAuth } = require("../middleware/auth");
+
+//Recipe Routes
+//Since linked from server js treat each path as:
+//recipe/:id, recipe/createRecipe, recipe/likeRecipe/:id
+
+router.get("/:id", ensureAuth, recipesController.getRecipe);
+
+// Enables user to create recipe w/ cloudinary for media uploads
+router.post("/createRecipe", upload.single("file"), recipesController.createRecipe);
+
+//Enables user to like recipe. In controller, uses RECIPE model to update likes by 1
+router.put("/likeRecipe/:id", recipesController.likeRecipe);
+
+//Enables user to delete recipe. In controller, uses RECIPE model to delete recipe from MongoDB collection
+router.delete("/deleteRecipe/:id", recipesController.deleteRecipe);
+
+module.exports = router;
